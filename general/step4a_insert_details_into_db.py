d0d44061004ede56050283f0458be1bfab7ea872
waiky8 <61842543+waiky8@users.noreply.github.com>
2020-04-03 17:59:14 +0100

    Update step4a_insert_details_into_db.py

--- general/step4a_insert_details_into_db.py
+++ general/step4a_insert_details_into_db.py
@@ -120,7 +120,7 @@ def main():
             r_gender = row[16]
             r_gender_post16 = row[17]
             r_religion = row[18]
-            r_other_urn = row[19].rstrip(".0")
+            r_other_urn = row[19].replace(".0", "")
             r_local_auth = row[20]
             r_website = ""        # populated in a separate module
 
